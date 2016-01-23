c56899f1f3fe067a80b59c7be9895c951f4871ba
Jonathan Huang & Karla <pair+jonwhuang.karlaking@devbootcamp.com>
2016-01-23 13:53:36 -0800

    Add map to page :neckbeard:

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,9 @@
 class EventsController < ApplicationController
-
   def index
+    @events = Event.all
+    # respond_to do |format|
+    #   format.html
+    #   format.json { render json: # some JSON data }
+    # end
   end
-
 end
